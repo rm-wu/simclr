e1954cd57b8150fd221e8793a33866ac5766827f
Cagatay <cagatay.yildiz1@gmail.com>
2024-10-28 12:27:13 +0100

    update

--- large_scale_object_embedding.py
+++ large_scale_object_embedding.py
@@ -8,6 +8,23 @@ import torch
 from ssl_libs.load_model import load_model, compute_features
 from embed_utils         import compute_embeddings
 
+class PadToMultipleOf14:
+    def __call__(self, image):
+        # Ensure the input is a PIL image
+        if not isinstance(image, Image.Image):
+            raise TypeError("Input image must be a PIL image")
+        width, height = image.size
+        # Calculate the nearest multiple of 14 that is greater than the current size
+        new_size = math.ceil(width / 14) * 14
+        # Calculate padding on each side to center the original image
+        pad_left = (new_size - width) // 2
+        pad_top = (new_size - height) // 2
+        pad_right = new_size - width - pad_left
+        pad_bottom = new_size - height - pad_top
+        # Apply padding using torchvision's functional.pad, with black padding (0)
+        padded_image = F.pad(image, (pad_left, pad_top, pad_right, pad_bottom), fill=0)
+        return padded_image
+
 class CustomImageDataset(Dataset):
     def __init__(self, root_dir, transform=None):
         self.root_dir = root_dir
@@ -41,12 +58,13 @@ MODEL_NAME = 'CLIP'
 RESULT_FOLDER = "object_embeddings"
 device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
 model  = load_model(MODEL_NAME)
-
 Path(RESULT_FOLDER).mkdir(parents=True, exist_ok=True)
 
-# Example usage
-# transform = transforms.Compose([transforms.ToTensor()])
-transform  = T.Compose([T.ToTensor(), T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))])
+if 'DINO' in MODEL_NAME:
+    transform  = T.Compose([T.ToTensor(), PadToMultipleOf14(), T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))])
+else:
+    transform  = T.Compose([T.ToTensor(), T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))])
+
 dataset    = CustomImageDataset(root_dir=DATA_DIR, transform=transform)
 dataloader = DataLoader(dataset, batch_size=32, shuffle=True, collate_fn=custom_collate_fn)
 
