9fe07be8cdef746d9b8869eb12cfda4226b46a6d
Cagatay <cagatay.yildiz1@gmail.com>
2025-01-21 23:15:49 +0100

    update

--- knn.py
+++ knn.py
@@ -43,7 +43,6 @@ def compute_knn(all_embeddings, all_labels, batch_size=256, normalize=True):
     misclassified_indices = []
 
     for start in range(0, num_embeddings, batch_size):
-        print(start)
         end = min(start + batch_size, num_embeddings)
         current_embeddings = all_embeddings[start:end] # batch_size,q
         similarity = torch.zeros(current_embeddings.shape[0], num_embeddings, device=all_embeddings.device)
