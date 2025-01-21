868bd854455b0cb4d2d594fd25698fba79eb2858
Cagatay <cagatay.yildiz1@gmail.com>
2025-01-21 22:15:34 +0100

    update

--- knn.py
+++ knn.py
@@ -31,99 +31,103 @@ def compute_knn(all_embeddings, all_labels, normalize=True):
     misclassified_idx = [i for i in range(similarity.shape[0]) if all_labels[i]!=all_labels[nearest_neighbors[i,0]]]
     return retrieval_rate, misclassified_idx
 
-VIDEO_FOLDER = 'videos_mae'
-VIDEO_NAMES  = os.listdir(VIDEO_FOLDER)
-device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
-# transform = T.Compose([T.Resize(224),T.CenterCrop(224),T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))])
-transform = T.Compose([T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))])
-imagenet_reverse_transform = T.Normalize(mean=(-0.485/0.229, -0.456/0.224, -0.406/0.225), std=(1/0.229, 1/0.224, 1/0.225))
+def main():
+    VIDEO_FOLDER = 'videos_mae'
+    VIDEO_NAMES  = os.listdir(VIDEO_FOLDER)
+    device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+    # transform = T.Compose([T.Resize(224),T.CenterCrop(224),T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))])
+    transform = T.Compose([T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))])
+    imagenet_reverse_transform = T.Normalize(mean=(-0.485/0.229, -0.456/0.224, -0.406/0.225), std=(1/0.229, 1/0.224, 1/0.225))
 
-# :read the embeddings
-saved_videos = os.listdir(VIDEO_FOLDER)
-all_embeddings = []
-all_labels     = []
-num_objects    = []
-loaded_videos  = []
-last_label     = -1
-for i,video in enumerate(saved_videos):
-    print(i,len(saved_videos))
-    if i==185:
-        continue
-    try:
-        frames, seg_maps, masks_per_object, embeddings = torch.load(f'{VIDEO_FOLDER}/{video}',map_location=device) # embeddings: [num_good_masks,N_,q]
-        class_labels = (last_label+1+torch.arange(embeddings.shape[0])).unsqueeze(0).repeat(embeddings.shape[1],1).T.flatten()
-        reshaped_embeddings = embeddings.reshape(-1,embeddings.shape[-1])
-        all_embeddings.append(reshaped_embeddings) # num_good_masks*N_,q
-        all_labels.append(class_labels) # num_good_masks*N_
-        last_label += embeddings.shape[0]
-        num_objects.append(embeddings.shape[0])
-        loaded_videos.append(video)
-    except Exception as e:
-        print(video, e)
-        continue
-
-all_labels = torch.cat(all_labels)
-all_embeddings = torch.cat(all_embeddings)
-num_objects = torch.tensor(num_objects)
-print(all_labels.shape, all_embeddings.shape, num_objects.shape, num_objects.sum())
-torch.save([all_labels, all_embeddings, num_objects, loaded_videos], f'{VIDEO_FOLDER}/videos_embeddings.pt')
-
-# all_labels, all_embeddings, num_objects, loaded_videos = torch.load(f'{VIDEO_FOLDER}/videos_embeddings.pt')
-
-retrieval_rate, misclassified_idx = compute_knn(all_embeddings, all_labels, normalize=True)
-
-transform = T.Compose([T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))])
-imagenet_reverse_transform = T.Normalize(mean=(-0.485/0.229, -0.456/0.224, -0.406/0.225), std=(1/0.229, 1/0.224, 1/0.225))
-def apply_transform(frames, transform):
-    ''' frames could be channel first or last'''
-    # assert frames.shape[-3]==3, 'channel dimension wrong'
-    if frames.shape[-3]!=3:
-        if frames.ndim==3:
-            frames = frames.permute(2,0,1) # 3, width, height
-            transformed_frames = transform(frames).permute(1,2,0) # 224, 224, 3
-        elif frames.ndim==4:
-            frames = frames.permute(0,3,1,2) # N, 3, width, height
-            transformed_frames = transform(frames).permute(0,2,3,1) # N, 224, 224, 3
-        elif frames.ndim==5:
-            frames = frames.permute(0,1,4,2,3) # N, T, 3, width, height
-            transformed_frames = transform(frames).permute(0,1,3,4,2) # N, T, 224, 224, 3
-    else:
-        transformed_frames = transform(frames)
-    return transformed_frames # [N,T,224,224,3] or [N,224,224,3]  or [224,224,3]
-
-def get_frame(anchor_idx):
-    video_id_anchor   = torch.where(num_objects.cumsum(0)>(anchor_idx//20))[0][0].item()
-    seg_map_id_anchor = anchor_idx//20 - num_objects.cumsum(0)[video_id_anchor-1].item() if anchor_idx//20>=num_objects[0] else anchor_idx//20
-    frame_id_anchor   = anchor_idx%20
-    video_name = loaded_videos[video_id_anchor]
-    print(video_id_anchor, seg_map_id_anchor, frame_id_anchor)
-    frames, seg_maps, masks_per_object, embeddings = torch.load(f'{VIDEO_FOLDER}/{video_name}',map_location=device)
-    video = Video(None, frames, seg_maps, transform)
-    video.masks_per_object = masks_per_object
-    frame = video.transformed_seg_cropped_imgs[seg_map_id_anchor][frame_id_anchor]
-    del video
-    return apply_transform(frame,imagenet_reverse_transform).cpu(), video_id_anchor, seg_map_id_anchor, frame_id_anchor
-
-for j in range(50):
-    if f'{j}.png' in os.listdir(f'{VIDEO_FOLDER}/errors'):
-        continue
-    fig,ax = plt.subplots(6,10,figsize=(30,18),squeeze=False)
-    for i in range(j*10,(j+1)*10):
-        anchor_idx = misclassified_idx[i]
-        anchor_frame, video_id, seg_map_id, frame_id = get_frame(anchor_idx)
-        if video_id==194 and seg_map_id==6 and frame_id==2:
+    # :read the embeddings
+    saved_videos = os.listdir(VIDEO_FOLDER)
+    all_embeddings = []
+    all_labels     = []
+    num_objects    = []
+    loaded_videos  = []
+    last_label     = -1
+    for i,video in enumerate(saved_videos):
+        print(i,len(saved_videos))
+        if i==185:
             continue
-        ax[0,i%10].imshow(anchor_frame);
-        ax[0,i%10].set_title(f'Anchor (video {video_id}, map {seg_map_id}, fr. {frame_id*20})');
-        ax[0,i%10].axis('off');
-        for k in range(5):
-            nn_frame, video_id, seg_map_id, frame_id = get_frame(nearest_neighbors[anchor_idx,k].item())
+        try:
+            frames, seg_maps, masks_per_object, embeddings = torch.load(f'{VIDEO_FOLDER}/{video}',map_location=device) # embeddings: [num_good_masks,N_,q]
+            class_labels = (last_label+1+torch.arange(embeddings.shape[0])).unsqueeze(0).repeat(embeddings.shape[1],1).T.flatten()
+            reshaped_embeddings = embeddings.reshape(-1,embeddings.shape[-1])
+            all_embeddings.append(reshaped_embeddings) # num_good_masks*N_,q
+            all_labels.append(class_labels) # num_good_masks*N_
+            last_label += embeddings.shape[0]
+            num_objects.append(embeddings.shape[0])
+            loaded_videos.append(video)
+        except Exception as e:
+            print(video, e)
+            continue
+
+    all_labels = torch.cat(all_labels)
+    all_embeddings = torch.cat(all_embeddings)
+    num_objects = torch.tensor(num_objects)
+    print(all_labels.shape, all_embeddings.shape, num_objects.shape, num_objects.sum())
+    torch.save([all_labels, all_embeddings, num_objects, loaded_videos], f'{VIDEO_FOLDER}/videos_embeddings.pt')
+
+    # all_labels, all_embeddings, num_objects, loaded_videos = torch.load(f'{VIDEO_FOLDER}/videos_embeddings.pt')
+
+    retrieval_rate, misclassified_idx = compute_knn(all_embeddings, all_labels, normalize=True)
+
+    transform = T.Compose([T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))])
+    imagenet_reverse_transform = T.Normalize(mean=(-0.485/0.229, -0.456/0.224, -0.406/0.225), std=(1/0.229, 1/0.224, 1/0.225))
+    def apply_transform(frames, transform):
+        ''' frames could be channel first or last'''
+        # assert frames.shape[-3]==3, 'channel dimension wrong'
+        if frames.shape[-3]!=3:
+            if frames.ndim==3:
+                frames = frames.permute(2,0,1) # 3, width, height
+                transformed_frames = transform(frames).permute(1,2,0) # 224, 224, 3
+            elif frames.ndim==4:
+                frames = frames.permute(0,3,1,2) # N, 3, width, height
+                transformed_frames = transform(frames).permute(0,2,3,1) # N, 224, 224, 3
+            elif frames.ndim==5:
+                frames = frames.permute(0,1,4,2,3) # N, T, 3, width, height
+                transformed_frames = transform(frames).permute(0,1,3,4,2) # N, T, 224, 224, 3
+        else:
+            transformed_frames = transform(frames)
+        return transformed_frames # [N,T,224,224,3] or [N,224,224,3]  or [224,224,3]
+
+    def get_frame(anchor_idx):
+        video_id_anchor   = torch.where(num_objects.cumsum(0)>(anchor_idx//20))[0][0].item()
+        seg_map_id_anchor = anchor_idx//20 - num_objects.cumsum(0)[video_id_anchor-1].item() if anchor_idx//20>=num_objects[0] else anchor_idx//20
+        frame_id_anchor   = anchor_idx%20
+        video_name = loaded_videos[video_id_anchor]
+        print(video_id_anchor, seg_map_id_anchor, frame_id_anchor)
+        frames, seg_maps, masks_per_object, embeddings = torch.load(f'{VIDEO_FOLDER}/{video_name}',map_location=device)
+        video = Video(None, frames, seg_maps, transform)
+        video.masks_per_object = masks_per_object
+        frame = video.transformed_seg_cropped_imgs[seg_map_id_anchor][frame_id_anchor]
+        del video
+        return apply_transform(frame,imagenet_reverse_transform).cpu(), video_id_anchor, seg_map_id_anchor, frame_id_anchor
+
+    for j in range(50):
+        if f'{j}.png' in os.listdir(f'{VIDEO_FOLDER}/errors'):
+            continue
+        fig,ax = plt.subplots(6,10,figsize=(30,18),squeeze=False)
+        for i in range(j*10,(j+1)*10):
+            anchor_idx = misclassified_idx[i]
+            anchor_frame, video_id, seg_map_id, frame_id = get_frame(anchor_idx)
             if video_id==194 and seg_map_id==6 and frame_id==2:
                 continue
-            ax[k+1,i%10].imshow(nn_frame);
-            ax[k+1,i%10].set_title(f'NN (video {video_id}, map {seg_map_id}, fr. {frame_id*20})');
-            ax[k+1,i%10].axis('off');
-    plt.tight_layout()
-    plt.show()
-    plt.savefig(f'{VIDEO_FOLDER}/errors/{j}.png',dpi=200)
-    plt.close()
+            ax[0,i%10].imshow(anchor_frame);
+            ax[0,i%10].set_title(f'Anchor (video {video_id}, map {seg_map_id}, fr. {frame_id*20})');
+            ax[0,i%10].axis('off');
+            for k in range(5):
+                nn_frame, video_id, seg_map_id, frame_id = get_frame(nearest_neighbors[anchor_idx,k].item())
+                if video_id==194 and seg_map_id==6 and frame_id==2:
+                    continue
+                ax[k+1,i%10].imshow(nn_frame);
+                ax[k+1,i%10].set_title(f'NN (video {video_id}, map {seg_map_id}, fr. {frame_id*20})');
+                ax[k+1,i%10].axis('off');
+        plt.tight_layout()
+        plt.show()
+        plt.savefig(f'{VIDEO_FOLDER}/errors/{j}.png',dpi=200)
+        plt.close()
+
+if __name__ == '__main__':
+    main()
