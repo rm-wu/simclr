5da43da2061d7b8a1b94b6a580314b77f9bb769f
rm-wu <riccardomereu5@gmail.com>
2025-01-23 12:33:30 +0200

    add: couple of changes while debugging the code

--- dense_prediction/open-hummingbird-eval/jobs/run_cribo_eval_dino.sh
+++ dense_prediction/open-hummingbird-eval/jobs/run_cribo_eval_dino.sh
@@ -34,3 +34,5 @@ echo "Patch Size: $patch_i"
 echo "Batch Size: $batch_i"
 
 python eval.py --seed 42 --batch-size $batch_i --input-size $img_i --patch-size $patch_i --memory-size $mem_i --embeddings-size $emb_i --data-dir data --model $model_i
+
+python eval.py --seed 42 --batch-size=64 --input-size=512 --patch-size=16 --memory-size=-1 --embeddings-size=384 --data-dir=data/ --model=dino_vits16
