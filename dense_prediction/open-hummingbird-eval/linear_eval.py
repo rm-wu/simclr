8dd5946934f6860a27de3c003d2bd0dcb046b409
rm-wu <riccardomereu5@gmail.com>
2025-01-23 18:24:14 +0200

    add: first version of the linear segmentation code

---
+++ dense_prediction/open-hummingbird-eval/linear_eval.py
@@ -0,0 +1,94 @@
+import argparse
+from datetime import datetime
+import torch
+from pathlib import Path
+
+from eval import seed_everything
+from src.ibot_vision_transformer import get_ibot_model_by_name
+from src.linear_eval import ls_finetune
+from src.image_transformations import Compose, RandomResizedCrop, RandomHorizontalFlip, Resize
+
+
+def main(args):
+    print(f"Linear Segmentation arguments: {args}")
+
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+    ## Load pretrained model
+    if args.model.startswith("dinov2"):
+        # TODO: this part needs to be checked to see if it can handle also the registers
+        model = torch.hub.load("facebookresearch/dinov2", args.model)
+    elif args.model.startswith("dino"):
+        model = torch.hub.load("facebookresearch/dino:main", args.model)
+    elif args.model.startswith("ibot"):
+        model = get_ibot_model_by_name(args.model)
+    else:
+        raise ValueError(f'Model "{args.model}" not recognized')
+    model = model.to(device)
+
+    if args.model.startswith("dinov2"):
+
+        def token_features(model, imgs):
+            return model.get_intermediate_layers(imgs)[0], None
+
+    else:
+
+        def token_features(model, imgs):
+            return model.get_intermediate_layers(imgs)[0][:, 1:], None
+
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    args.out_dir = (
+        Path(args.out_dir)
+        / args.dataset_name
+        / f"{args.model}_i{args.input_size}_p{args.patch_size}_e{args.embeddings_size}_b{args.batch_size}_s{args.seed}"
+        / timestamp
+    )
+    args.out_dir.mkdir(parents=True, exist_ok=True)
+    ls_finetune(
+        backbone=model,
+        patch_size=args.patch_size,
+        head_type="linear",
+        max_epochs=args.max_epochs,
+        lr=args.lr,
+        decay_rate=0.1,
+        drop_at=args.drop_at,
+        batch_size=args.batch_size,
+        dataset_name=args.dataset_name,
+        data_dir=args.data_dir,
+        num_workers=args.num_workers,
+        feat_extr_fn=token_features,
+        input_size=args.input_size,
+        train_mask_size=100,
+        val_mask_size=100,
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Linear Segmentation Evaluation")
+    parser.add_argument("--seed", type=int, default=42)
+
+    parser.add_argument("--model", type=str, required=True)
+    parser.add_argument("--input-size", type=int, default=448)
+    parser.add_argument("--embeddings-size", type=int, required=True)
+    parser.add_argument("--batch-size", type=int, default=128)
+    parser.add_argument("--patch-size", type=int, default=14)
+
+    parser.add_argument("--lr", type=float, default=0.01)
+    parser.add_argument("--max-epochs", type=int, default=20)
+    parser.add_argument(
+        "--drop_at", type=int, default=20
+    )  # TODO: check what is this about
+
+    parser.add_argument("--dataset-name", type=str, default="voc", choices=["voc"])
+    parser.add_argument("--data-dir", type=str, default="data/")
+    parser.add_argument("--out-dir", type=str, default="outputs/linear_eval/")
+    parser.add_argument("--num-workers", type=int, default=64)
+    parser.add_argument(
+        "--save-features",
+        action="store_true",
+        help="Whether to save the features and labels to the output directory",
+    )
+
+    args = parser.parse_args()
+    seed_everything(args.seed)
+    main(args)
---
+++ dense_prediction/open-hummingbird-eval/src/linear_eval.py
@@ -0,0 +1,265 @@
+# TODO: this part comes from hbird_eval needs to be refactored
+if __name__ == "__main__":
+    # Add project root to path if running this file as a main
+    import sys
+    import pathlib
+
+    p = str(pathlib.Path(__file__).parent.resolve()) + "/"
+    sys.path.append(p)
+
+
+import torch
+from torch import nn
+from torch.optim.lr_scheduler import StepLR
+import torchvision.transforms as T
+from torchvision.transforms.functional import InterpolationMode
+from pathlib import Path
+import random
+import os
+from typing import Callable
+import einops as ein
+from tqdm import tqdm
+
+from src.voc_data import VOCDataModule
+from src.ade20kdata import Ade20kDataModule
+
+from src.image_transformations import Compose, RandomResizedCrop, RandomHorizontalFlip, Resize
+from src.ls_transforms import SepTransforms
+from src.image_transformations import CombTransforms
+
+
+class LinearFinetune(nn.Module):
+    def __init__(
+        self,
+        patch_size: int,
+        num_classes: int,
+        lr: float,
+        input_size: int,
+        spatial_res: int,
+        val_iters: int,
+        drop_at: int,
+        backbone: nn.Module,
+    ):
+        super().__init__()
+        self.backbone = backbone
+        self.head = nn.Conv2d(self.backbone.embed_dim, num_classes, 1)
+
+    def forward(self):
+        pass
+
+
+def ls_finetune(
+    backbone: nn.Module,
+    feat_extr_fn: Callable,
+    patch_size: int,
+    head_type: str,
+    max_epochs: int,
+    lr: float,
+    decay_rate: float,
+    drop_at: int,
+    batch_size: int,
+    dataset_name: str,
+    data_dir: Path,
+    num_workers: int = 32,
+    input_size: int = 448,
+    train_mask_size: int = 100,
+    val_mask_size: int = 100,
+):
+    # TODO: These hyperparameters should be checked with the original code
+    # input_size = args.input_size # 448
+    # train_mask_size = 100
+    # val_mask_size = 100
+
+    img_train_transforms = Compose(
+        [
+            RandomHorizontalFlip(p=0.5),
+            T.ToTensor(),
+            T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    )
+    shared_train_transform = Compose(
+        [RandomResizedCrop(size=input_size, scale=(0.8, 1.0))]
+    )
+
+    img_val_transforms = T.Normalize(
+        mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+    )
+    shared_val_transform = Compose(
+        [
+            Resize((input_size, input_size)),
+            T.ToTensor(),
+        ]
+    )
+    val_target_transforms = Compose(
+        [
+            Resize((input_size, input_size), interpolation=InterpolationMode.NEAREST),
+            T.ToTensor(),
+        ]
+    )
+    train_transforms = CombTransforms(
+        img_transform=img_train_transforms,
+        tgt_transform=None,
+        img_tgt_transform=shared_train_transform,
+    )
+    val_transforms = CombTransforms(
+        img_transform=img_val_transforms,
+        tgt_transform=None,
+        img_tgt_transform=shared_val_transform,
+    )
+
+    if dataset_name == "voc":
+        num_classes = 21
+        ignore_index = 255
+        data_module = VOCDataModule(
+            batch_size=batch_size,
+            return_masks=True,
+            num_workers=num_workers,
+            train_split="trainaug",
+            val_split="val",
+            data_dir=data_dir,
+            train_image_transform=train_transforms,
+            drop_last=True,
+            val_image_transform=val_transforms,
+            val_target_transform=val_target_transforms,
+        )
+    # TODO: This part is missing since the open-hummingbird-eval does not support
+    # COCO. But it should be possible to integrate that from NeCo repository maybe.
+    # elif "coco" in dataset_name:
+    #     assert len(dataset_name.split("-")) == 2
+    #     mask_type = dataset_name.split("-")[-1]
+    #     assert mask_type in ["thing", "stuff"]
+    #     if mask_type == "thing":
+    #         num_classes = 12
+    #     else:
+    #         num_classes = 15
+    #     ignore_index = 255
+    #     file_list = os.listdir(os.path.join(data_dir, "images", "train2017"))
+    #     file_list_val = os.listdir(os.path.join(data_dir, "images", "val2017"))
+    #     random.shuffle(file_list_val)
+    #     # sample 10% of train images
+    #     random.shuffle(file_list)
+    #     file_list = file_list[:int(len(file_list)*0.1)]
+    #     print(f"sampled {len(file_list)} COCO images for training")
+
+    #     data_module = CocoDataModule(batch_size=train_config["batch_size"],
+    #                                  num_workers=_config["num_workers"],
+    #                                  file_list=file_list,
+    #                                  data_dir=data_dir,
+    #                                  file_list_val=file_list_val,
+    #                                  mask_type=mask_type,
+    #                                  train_transforms=train_transforms,
+    #                                  val_transforms=val_image_transforms,
+    #                                  val_target_transforms=val_target_transforms)
+    elif dataset_name == "ade20k":
+        # TODO: Evaluate its correctness
+        num_classes = 151
+        ignore_index = 0
+        val_transforms = SepTransforms(val_image_transforms, val_target_transforms)
+        data_module = Ade20kDataModule(
+            data_dir,
+            train_transforms=train_transforms,
+            val_transforms=val_transforms,
+            shuffle=False,
+            num_workers=num_workers,
+            batch_size=batch_size,
+        )
+    else:
+        raise ValueError(f"{dataset_name} not supported")
+
+    # Init Method
+    spatial_res = input_size / patch_size
+    assert spatial_res.is_integer()
+
+    linear_head = nn.Conv2d(backbone.embed_dim, num_classes, 1)
+
+    # freeze all layers of backbone
+    for param in backbone.parameters():
+        param.requires_grad = False
+    backbone.eval()
+
+    optimizer = torch.optim.SGD(
+        linear_head.parameters(), weight_decay=1e-4, momentum=0.9, lr=lr
+    )
+    scheduler = StepLR(optimizer, gamma=decay_rate, step_size=drop_at)
+
+    data_module.setup()
+    dataset_size = data_module.get_train_dataset_size()
+    num_classes = data_module.get_num_classes()
+    train_loader = data_module.train_dataloader()
+    val_loader = data_module.val_dataloader()
+
+    for epoch in range(max_epochs):
+        for batch in tqdm(train_loader):
+            images, masks = batch
+            B, C, H, W = images.shape
+            assert H == W == input_size
+
+            with torch.no_grad():
+                tokens = feat_extr_fn(backbone, images)
+                tokens = ein.rearrange(
+                    tokens, "b (h w) d -> b d h w", h=spatial_res, w=spatial_res
+                )
+                tokens = nn.functional.interpolate(
+                    tokens, size=(train_mask_size, train_mask_size), mode="bilinear"
+                )
+
+            optimizer.zero_grad()
+            outputs = linear_head(images)
+            loss = nn.CrossEntropyLoss()(outputs, masks.long().squeeze())
+            loss.backward()
+            optimizer.step()
+            scheduler.step()
+    # model = LinearFinetune(
+    #     patch_size=patch_size,
+    #     head_type=head_type,
+    #     backbone=backbone,
+    #     # arch_version=train_config.get("arch_version"),
+    #     num_classes=num_classes,
+    #     lr=lr,
+    #     input_size=input_size,
+    #     spatial_res=int(spatial_res),
+    #     val_iters=val_iters,
+    #     decay_rate=decay_rate if decay_rate is not None else 0.1,
+    #     drop_at=drop_at,
+    #     ignore_index=ignore_index,
+    # )
+
+    # # Optionally load weights
+    # if not restart:
+    #     weights = get_backbone_weights(
+    #         arch, method, patch_size=patch_size, ckpt_path=train_config.get("ckpt_path")
+    #     )
+    #     msg = model.load_state_dict(weights, strict=False)
+    #     print(msg)
+
+    # # Init checkpoint callback storing top 3 heads
+    # checkpoint_dir = os.path.join(
+    #     train_config["ckpt_dir"], _run.experiment_info["name"]
+    # )
+    # checkpoint_callback = ModelCheckpoint(
+    #     dirpath=checkpoint_dir,
+    #     monitor="miou_val",
+    #     filename="ckp-{epoch:02d}-{miou_val:.4f}",
+    #     save_top_k=3,
+    #     mode="max",
+    #     verbose=True,
+    # )
+
+    # # Init trainer and start training head
+    # trainer = Trainer(
+    #     num_sanity_val_steps=0,
+    #     logger=neptune_logger,
+    #     max_epochs=train_config["max_epochs"],
+    #     gpus=_config["gpus"],
+    #     accelerator="ddp" if _config["gpus"] > 1 else None,
+    #     fast_dev_run=train_config["fast_dev_run"],
+    #     log_every_n_steps=50,
+    #     benchmark=True,
+    #     deterministic=False,
+    #    resume_from_checkpoint=train_config["ckpt_path"] if restart else None,
+    #     amp_backend="native",
+    #     terminate_on_nan=True,
+    #     callbacks=[checkpoint_callback],
+    # )
+    # trainer.fit(model, datamodule=data_module)
+    # pass
