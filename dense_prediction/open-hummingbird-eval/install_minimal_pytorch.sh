1cc0c7c3874ab9c42df8d3e0bdd1f7ce0e1d038e
Riccardo Mereu <riccardo.mereu@aalto.fi>
2025-01-27 12:11:56 +0200

    update: LUMI enviroment

--- dense_prediction/open-hummingbird-eval/install_minimal_pytorch.sh
+++ dense_prediction/open-hummingbird-eval/install_minimal_pytorch.sh
@@ -2,4 +2,11 @@
 module purge
 module use /appl/local/training/modules/AI-20241126
 module load cotainr
+module load singularity-userfilesystems
 # cotainr build python312.sif --system=lumi-g --conda-env=python312.yml
+cotainr build lumi_env.sif --system=lumi-g --conda-env=minimal_env.yml
+singularity shell lumi_env.sif
+
+
+srun --overlap --pty --jobid=<jobid> $SHELL
+
