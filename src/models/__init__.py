68daad7555e83560971db9e90b85e84d69253e37
rm-wu <riccardomereu5@gmail.com>
2025-02-01 14:18:19 +0200

    update: fix small typo

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,3 @@
-from src.models.vision_transformer import get_ibot_model_by_name
-from src.models.cribo_transformer import get_cribo_model_by_name
+from src.models.ibot_vision_transformer import get_ibot_model_by_name
+from src.models.cribo_vision_transformer import get_cribo_model_by_name
 from src.models.models import *
